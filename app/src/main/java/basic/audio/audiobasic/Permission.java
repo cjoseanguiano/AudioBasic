6fca7a86181422a4000608fc1ea46866f573c0db
Carlos Anguiano <c.joseanguiano@gmail.com>
2018-01-13 20:15:29 -0600

    AudioBasic

---
+++ app/src/main/java/basic/audio/audiobasic/Permission.java
@@ -0,0 +1,20 @@
+package basic.audio.audiobasic;
+
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+
+/**
+ * Created by Carlos Anguiano on 13/01/18.
+ * For more info contact: c.joseanguiano@gmail.com
+ */
+
+public class Permission {
+    public static void requestPermission(Activity activity, String permission) {
+        if (ContextCompat.checkSelfPermission(activity, permission)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(activity, new String[]{permission}, 0);
+        }
+    }
+}
